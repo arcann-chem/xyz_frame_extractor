e77557fa8a6fb252dd234080e554729a885ab15b
Rolf David <12892294+cdavro@users.noreply.github.com>
2023-08-14 12:51:19 +0200

    updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="xyz_frame_extractor",
-    version="0.1.0",
+    version="0.1.2",
     author="cdavro",
     author_email="cdavro+github_contact@slmail.me",
     description="to_do",
