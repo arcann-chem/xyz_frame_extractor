2c9c9a339bea9dddfe79788bf4cdd444585e0a71
Rolf David <12892294+cdavro@users.noreply.github.com>
2024-05-15 10:48:24 +0200

    Update version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="xyz_frame_extractor",
-    version="0.2.0",
+    version="1.0.0",
     author="cdavro",
     author_email="cdavro.arcann.yi9jv@eridanitech.com",
     description="to_do",
@@ -10,15 +10,15 @@ setup(
     license="GNU Affero General Public License v3",
     packages=find_packages(),
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     python_requires=">=3.7",
     install_requires=["numpy>=1.17.3"],
